42b2a8f7a1d52ea1483bdc8c69aa8682ff431c54
Sherif Kamal <sherif.kamal.salem@gmail.com>
2022-05-17 20:08:31 +0200

    Update FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
    Co-authored-by: danillahtin <danil_lahtin@mail.ru>

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -25,7 +25,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			case let .success((data, response)):
 				completion(FeedImagesMapper.map(data, response))
 			case .failure:
-				completion(.failure(.connectivity as Error))
+				completion(.failure(Error.connectivity))
 			}
 		}
 	}
